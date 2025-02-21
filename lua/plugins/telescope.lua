6547dbc9476590c718d835de2870083ee756e85b
Jiro <j.code.public@gmail.com>
2025-02-20 16:51:37 -0800

    Add shortcut for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
   keys = {
     { "<leader>p", "<cmd>Telescope find_files<cr>", desc = "Telescope find_files" },
     { "<leader>g", "<cmd>Telescope live_grep<cr>", desc = "Telescope live_greps" },
+    { "<leader>r", "<cmd>Telescope resume<cr>", desc = "Telescope Resume" },
     { "<leader>b", "<cmd>Telescope pickers<cr>", desc = "Telescope Previous Picker" },
     { "<leader>f", "<cmd>Telescope quickfix<cr>", desc = "Telescope Quixfix" },
     { "<leader>H", "<cmd>Telescope help_tugs<cr>", desc = "Telescope Help Tags" },
