942c77fa9b30daa581c18bc94de44f5d1c3480c4
Jiro <j.code.public@gmail.com>
2026-03-27 14:09:32 +0900

    update lazy

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,43 +1,38 @@
 return {
   'nvim-lualine/lualine.nvim',
-  config = function()
-    -- local theme = require 'lualine.themes.tokyonight'
-    -- theme.normal.c.bg = nil
-
-    require('lualine').setup({
-      sections = {
-        lualine_a = { 'mode' },
-        lualine_b = {
-          {
-            'filename',
-            file_status = true,
-            path = 1,
-            shorting_target = 40,
-          },
-          'diagnostics',
+  opts = {
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = {
+        {
+          'filename',
+          file_status = true,
+          path = 1,
+          shorting_target = 40,
         },
-        lualine_c = { 'branch' },
-        lualine_x = { 'encoding', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' },
+        'diagnostics',
       },
-      options = {
-        theme = 'nord',
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {
-          {
-            'filename',
-            file_status = true,
-            path = 1,
-            shorting_target = 40,
-          },
+      lualine_c = { 'branch' },
+      lualine_x = { 'encoding', 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' },
+    },
+    options = {
+      theme = 'nord',
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {
+        {
+          'filename',
+          file_status = true,
+          path = 1,
+          shorting_target = 40,
         },
-        lualine_x = { 'location' },
-        lualine_y = {},
-        lualine_z = {}
       },
-    })
-  end
+      lualine_x = { 'location' },
+      lualine_y = {},
+      lualine_z = {}
+    },
+  }
 }
